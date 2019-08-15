840163d4772911289ffba956be98839c7d5cb349
Alamu Ramasamy <alamuroxx@gmail.com>
2019-08-15 07:57:29 +0400

    Last

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,10 @@ class ArticlesController < ApplicationController
 
 	def index
 		if params[:category].blank?
-			@articles = Article.all.order("created_at ASC")
+			@articles = Article.all.order("created_at DESC")
 		else
 			@category_id = Category.find_by(name: params[:category]).id
-			@articles = Article.where(category_id: @category_id).order("created_at ASC") 
+			@articles = Article.where(category_id: @category_id).order("created_at DESC") 
 		end
 	end
 
